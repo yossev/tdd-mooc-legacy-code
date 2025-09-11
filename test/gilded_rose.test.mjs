2e57f883d2bdab14c6cc9c601ab87e4520c64f4f
Youssef Hesham <synthpyt@gmail.com>
2025-09-11 04:24:03 +0300

    Added some extra tests

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -2,7 +2,7 @@ import { describe, test } from "vitest";
 import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
-describe("Gilded Rose", () => {
+describe("Gilded Rose - Normal Item", () => {
   test("foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
@@ -20,4 +20,31 @@ describe("Gilded Rose", () => {
     gildedRose.updateQuality();
     expect(gildedRose.items[0].quality).toBe(0);
   })
+
+  test("quality decreased by 2 when sellIn < 0", () => {
+    const gildedRose = new Shop([new Item("Normal item", 0, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(8);
+  })
 });
+
+describe("Aged Brie", () => {
+
+  test("Aged brie quality increases by 1 before SellIn date", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 2)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(3);
+  })
+
+  test("Aged brie gets twice in quality as older that it gets, doubles when sellIn date is passed", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 2)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(4);
+  })
+
+  test("Aged brie cant go above 50", () =>{
+        const gildedRose = new Shop([new Item("Aged Brie", 0, 50)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(50);
+  })
+})
