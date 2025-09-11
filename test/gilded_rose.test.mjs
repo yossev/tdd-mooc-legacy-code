188a23c489ddeb5104637d2dd0f79d371d3208ab
Youssef Hesham <synthpyt@gmail.com>
2025-09-11 03:58:55 +0300

    Added tests for normal items, does not go below 0

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -8,4 +8,16 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
+
+  test("reduces quality by 1 for normal item with quality above 0", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0)
+  })
+
+  test("does not reduce items below 0", () => {
+    const gildedRose = new Shop([new Item("Normal item", 0, 1)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+  })
 });
