d63b188f80ca5e0b5c4414e5b3a0592c3f0b6bfc
Youssef Hesham <synthpyt@gmail.com>
2025-09-14 19:27:35 +0300

    refac

--- src/GildedRose.mjs
+++ src/GildedRose.mjs
@@ -14,7 +14,11 @@ class QualityStrategy {
 
 class NormalItemStrategy extends QualityStrategy {
     update(item) {
-        item.quality = Math.max(0, item.quality - 1);
+        if(item.sellIn < 0) {
+            item.quality = Math.max(0, item.quality - 2);
+        } else {
+            item.quality = Math.max(0, item.quality - 1);
+        }
     }
 }
 
--- test/refac_gilded_rose.test.mjs
+++ test/refac_gilded_rose.test.mjs
@@ -4,8 +4,29 @@ import { GildedRose, Item } from "../src/GildedRose.mjs";
 
 describe("Normal Item Tests", () => {
     test("Name does not change", () => {
-        const shop = new GildedRose([new Item("Normal Item", 1, 1)]);
-        const items = shop.updateQuality();
+        const items = [new Item("Normal Item", 10, 20)];
+        const shop = new GildedRose(items);
         expect(items[0].name).to.equal("Normal Item")
+    })
+
+    test("Reduces quality by 1 for normal item with quality above 0", () => {
+        const items = [new Item("Normal Item", 10, 20)];
+        const shop = new GildedRose(items);
+        shop.updateQuality();
+        expect(items[0].quality).to.equal(19);
+    }) 
+
+    test("Does not reduce items below 0", () => {
+        const items = [new Item("Normal Item", 10, 0)];
+        const shop = new GildedRose(items);
+        shop.updateQuality();
+        expect(items[0].quality).to.equal(0);
+    })
+
+    test("quality decreased by 2 when sellIn < 0", () => {
+        const items = [new Item("Normal Item", -1, 20)];
+        const shop = new GildedRose(items);
+        shop.updateQuality();
+        expect(items[0].quality).to.equal(18);
     })
 })
